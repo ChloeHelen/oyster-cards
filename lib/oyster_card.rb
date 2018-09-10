0a68a53c652d334e9f5ba25cb55d4108a6104a66
Chloe Mingay <chloemingay@MacBook-Pro-3.local>
2018-09-10 15:56:47 +0100

    Introduces minimum fare

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,13 +1,16 @@
 class Oystercard
 
   attr_reader :balance
+  attr_writer :in_journey
 
   DEFAULT_VALUE = 0
   MAXIMUM_VALUE = 90
+  MINIMUM_FARE = 1
 
   def initialize(balance = DEFAULT_VALUE)
     raise "Maximum amount allowed is #{MAXIMUM_VALUE}" if balance > MAXIMUM_VALUE
     @balance = balance
+    @in_journey = false
   end
 
   def topup(amount)
@@ -17,6 +20,25 @@ class Oystercard
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    raise "Insufficient Funds" if insufficient_balance
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  private
+
+  def insufficient_balance
+    @balance < MINIMUM_FARE
   end
 
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,7 +1,7 @@
 require 'oyster_card.rb'
 
 describe Oystercard do
-
+  it { is_expected.to respond_to :in_journey?}
   it "is initialized with a balance of 0" do
     card = Oystercard.new
     expect(card.balance).to eq 0
@@ -18,6 +18,10 @@ describe Oystercard do
       expect(subject.balance).to eq 4
     end
 
+    # before (:each) do
+    #   subject.topup(described_class::MINIMUM_FARE)
+    # end
+
     it "raises error when balance goes beyond the maximum amount" do
       expect{subject.topup(described_class::MAXIMUM_VALUE + 1)}.to raise_error "Maximum amount allowed is #{described_class::MAXIMUM_VALUE}"
     end
@@ -33,5 +37,31 @@ describe Oystercard do
     end
 
   end
+
+  describe "#touch_in" do
+
+    it "changes in_journey to equal true" do
+      subject.topup(10)
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+
+    it "raise an error when the balance is less than the minimum fare" do
+      expect{subject.touch_in}.to raise_error "Insufficient Funds"
+    end
+
+  end
+
+  describe "#touch_out" do
+
+    it "changes in_journey to equal false" do
+      subject.topup(10)
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+
+  end
+
 
 end
