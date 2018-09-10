1933359c8135dd1074c6733b425cad5ef18b5244
Chloe Mingay <chloemingay@MacBook-Pro-3.local>
2018-09-10 15:03:29 +0100

    Oyster card now has a maximum limit

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -3,12 +3,15 @@ class Oystercard
   attr_reader :balance
 
   DEFAULT_VALUE = 0
+  MAXIMUM_VALUE = 90
 
   def initialize(balance = DEFAULT_VALUE)
+    raise "Maximum amount allowed is #{MAXIMUM_VALUE}" if balance > MAXIMUM_VALUE
     @balance = balance
   end
 
   def topup(amount)
+    raise "Maximum amount allowed is #{MAXIMUM_VALUE}" if @balance + amount > MAXIMUM_VALUE
     @balance += amount
   end
 
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,17 +1,29 @@
 require 'oyster_card.rb'
 
 describe Oystercard do
+
   it "is initialized with a balance of 0" do
     card = Oystercard.new
     expect(card.balance).to eq 0
   end
 
+  it "cannot be initialized with a balance greater than maximum allowed" do
+    expect{Oystercard.new(described_class::MAXIMUM_VALUE + 1)}.to raise_error "Maximum amount allowed is #{described_class::MAXIMUM_VALUE}"
+  end
+
   describe "#topup" do
+
     it "increases the balance by defined amount" do
       subject.topup(4)
       expect(subject.balance).to eq 4
     end
-    
+
+    it "raises error when balance goes beyond the maximum amount" do
+      expect{subject.topup(described_class::MAXIMUM_VALUE + 1)}.to raise_error "Maximum amount allowed is #{described_class::MAXIMUM_VALUE}"
+    end
+
+
+
   end
 
 end
