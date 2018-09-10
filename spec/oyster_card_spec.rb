ca01c6b88477d7e7df18b9dc26c6339f5fec23f4
Chloe Mingay <chloemingay@MacBook-Pro-3.local>
2018-09-10 15:25:17 +0100

    Deduct method added

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -22,7 +22,15 @@ describe Oystercard do
       expect{subject.topup(described_class::MAXIMUM_VALUE + 1)}.to raise_error "Maximum amount allowed is #{described_class::MAXIMUM_VALUE}"
     end
 
+  end
 
+  describe "#deduct" do
+
+    it "reduces the balance by a specified amount" do
+      subject.topup(10)
+      subject.deduct(10)
+      expect(subject.balance).to eq 0
+    end
 
   end
 
